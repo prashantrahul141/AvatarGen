cd649e1b887bbabcae5ca087484416fc726e5de0
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2022-12-26 12:34:21 +0530

    addings paths

--- src/constants/VALUES.py
+++ src/constants/VALUES.py
@@ -1,1 +1,10 @@
+import os
+
 PHOTO_SIZE = -1
+
+# paths
+CONSTANTS_DIR = os.path.dirname(__file__)
+SRC_DIR = os.path.split(CONSTANTS_DIR)[0]
+ROOT_DIR = os.path.split(SRC_DIR)[0]
+ASSETS_DIR = os.path.join(ROOT_DIR, 'assets')
+USER_GIVEN_IMAGES_DIR = os.path.join(ASSETS_DIR, 'user_given_images')
