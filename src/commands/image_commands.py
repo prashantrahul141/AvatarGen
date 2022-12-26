3ec4fad5df12bf1a0d30c95278536eb3a2fd50ea
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2022-12-26 17:01:41 +0530

    adding blur

--- src/commands/image_commands.py
+++ src/commands/image_commands.py
@@ -1,9 +1,9 @@
 # imports
 from telegram import Update
 from telegram.ext import ContextTypes
-from image_commands import _blur
+from .image_commands_impl import _blur
 
 
 async def blur(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
     '''blurs the image.'''
-    await _blur.blur(update, ctx)
+    await _blur._blur(update, ctx)
