8f2acf88d7b11ee54210e601532378dd3f54c0a4
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2022-12-27 00:11:20 +0530

    adding deleted logging, total decreasing delete time per image

--- src/utils/deleteImage.py
+++ src/utils/deleteImage.py
@@ -1,20 +1,29 @@
 import os
 from constants.VALUES import USER_GIVEN_IMAGES_DIR, EDITED_USER_GIVEN_IMAGES_DIR, DELETE_COROUTINE_TIME
 import time
+import logging
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 
 async def deleteImageCoroutine(context) -> None:
     # delete images coroutine
     # deletes images after a set amount of time they are created
 
+    _total_deleted = 0
     _files = os.listdir(USER_GIVEN_IMAGES_DIR)
     _current_unix_timestamp = time.time()
     for _file in _files:
         _file_path = os.path.join(USER_GIVEN_IMAGES_DIR, _file)
         try:
             _file_unix_timestamp = os.path.getmtime(_file_path)
-            if _current_unix_timestamp - _file_unix_timestamp > DELETE_COROUTINE_TIME:
+            if _current_unix_timestamp - _file_unix_timestamp > int(DELETE_COROUTINE_TIME/12):
                 os.remove(_file_path)
+                _total_deleted += 1
 
         except Exception:
             raise Exception
@@ -22,8 +31,11 @@ async def deleteImageCoroutine(context) -> None:
         _file_path = os.path.join(EDITED_USER_GIVEN_IMAGES_DIR, _file)
         try:
             _file_unix_timestamp = os.path.getmtime(_file_path)
-            if _current_unix_timestamp - _file_unix_timestamp > DELETE_COROUTINE_TIME:
+            if _current_unix_timestamp - _file_unix_timestamp > int(DELETE_COROUTINE_TIME/12):
                 os.remove(_file_path)
+                _total_deleted += 1
 
         except Exception:
             raise Exception
+
+    logger.info(f"Deleted {_total_deleted} files.")
