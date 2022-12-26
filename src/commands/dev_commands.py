88a58982d9a7097a11c00f03eff35600500a7fe5
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2022-12-27 00:49:25 +0530

    minor change

--- src/commands/dev_commands.py
+++ src/commands/dev_commands.py
@@ -7,7 +7,7 @@ from utils.deleteImage import deleteImageCoroutine
 
 async def test_getImage(update: Update, ctx: ContextTypes.DEFAULT_TYPE) -> None:
     '''To test getImage utils function.'''
-    _file_path: str | None = await getImage(update)
+    _file_path, _file_path = await getImage(update)
     if _file_path != None:
         with open(_file_path, 'rb') as f:
             await update.message.reply_photo(f)
