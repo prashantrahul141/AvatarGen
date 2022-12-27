cb675320df60f19fe2a264e742af004487fe8040
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2022-12-27 20:31:31 +0530

    adding deepfry command

--- src/commands/image_commands.py
+++ src/commands/image_commands.py
@@ -1,7 +1,7 @@
 # imports
 from telegram import Update
 from telegram.ext import ContextTypes
-from .image_commands_impl import _blur, _pixelate, _emojioverlay
+from .image_commands_impl import _blur, _pixelate, _emojioverlay, _deepfry
 
 
 async def blur(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
@@ -17,3 +17,8 @@ async def pixelate(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
 async def emojioverlay(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
     '''overlays an emoji over an image.'''
     await _emojioverlay._emojioverlay(update, ctx)
+
+
+async def deepfry(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
+    '''make deepfry meme from an image.'''
+    await _deepfry._deepfry(update, ctx)
