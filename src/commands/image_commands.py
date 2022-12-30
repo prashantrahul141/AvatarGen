855e874c2f4e87908b43b07dd08ff8838b4faec4
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2022-12-30 22:53:21 +0530

    improving doc strings

--- src/commands/image_commands.py
+++ src/commands/image_commands.py
@@ -11,12 +11,22 @@ async def blur(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
 
 
 async def pixelate(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
-    '''pixelates an images.'''
+    '''
+    pixelates an images.
+    usage:
+        /pixelate {scale}
+    scale: between 0 and 6 (including)
+    '''
     await _pixelate._pixelate(update, ctx)
 
 
 async def emojioverlay(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
-    '''overlays an emoji over an image.'''
+    '''
+    overlays an emoji over an image.
+    usage:
+        /emojioverlay {emoji}
+    emoji: any native emoji.
+    '''
     await _emojioverlay._emojioverlay(update, ctx)
 
 
@@ -26,12 +36,20 @@ async def deepfry(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
 
 
 async def caption1(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
-    '''adds captions to the upper part of the image.'''
+    '''
+    adds captions to the upper part of the image.
+    usage:
+        /caption1 {text here}
+    '''
     await _caption1._caption1(update, ctx)
 
 
 async def caption2(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
-    '''adds captions to the bottom part of the image.'''
+    '''
+    adds captions to the bottom part of the image.
+    usage:
+        /caption1 {text here}
+    '''
     await _caption2._caption2(update, ctx)
 
 
@@ -41,12 +59,21 @@ async def cropcircle(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
 
 
 async def rotate(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
-    '''rotates the image.'''
+    '''
+    rotates the image.
+    usage:
+        /rotate {angle}
+    angle: in angles
+    '''
     await _rotate._rotate(update, ctx)
 
 
 async def ascify(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
-    '''rotates the image.'''
+    '''
+    create ascii art from text.
+    usage:
+     /ascify {text}
+    '''
     await _ascify._ascify(update, ctx)
 
 
@@ -56,7 +83,12 @@ async def crop(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
 
 
 async def saturate(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
-    '''change saturation of an image.'''
+    '''
+    change saturation of an image.
+    usage:
+        /saturate {scale}
+    scale: between 0 - 10 (including)
+    '''
     await _saturate._saturate(update, ctx)
 
 
@@ -76,5 +108,5 @@ async def mirror(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
 
 
 async def invert(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
-    '''mirrors' an image vertically.'''
+    '''inverts the colors on an image..'''
     await _invert._invert(update, ctx)
