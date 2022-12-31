af8cffa7a3bd3a15acc415fa81b83daa1e38285a
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2022-12-31 15:51:46 +0530

    adding start command

--- src/commands/basic_commands.py
+++ src/commands/basic_commands.py
@@ -6,3 +6,9 @@ from telegram.ext import ContextTypes
 async def alive(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
     '''To check if bot is working.'''
     await update.message.reply_text('yes')
+
+
+async def start(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
+    '''To start the bot.'''
+    await update.message.reply_text(f'''Hello {update.effective_user.first_name if update.effective_user else ''}! \
+You can try /help to get a proper help message on how to use this bot.''')
